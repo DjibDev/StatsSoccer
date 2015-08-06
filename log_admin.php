2a1c34ae89a3f0a937be5abcc0c52822f2eee557
Jerome BLOT <galaxymuse@gmail.com>
2015-08-06 14:39:30 +0200

    creation du formulaire de log Admin

---
+++ log_admin.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+	<title>Les statistiques</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="style_base.css"/>
+	</head>
+
+<body>
+	<p>Veuillez entrer le mot de passe pour obtenir les droits d'administation</p>
+
+	<form action="administrer.php" method="post">
+	<p>
+		<input type="password" name="mot_de_passe" />
+		<input type="submit" value="Valider" />
+	</p>
+	</form>
+
+</body>
+</html> 
