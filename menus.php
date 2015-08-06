3ca38165c5aae851949674bd5b0314ec3e43abeb
Jerome BLOT <galaxymuse@gmail.com>
2015-08-06 14:39:49 +0200

    creation du fichier menu

---
+++ menus.php
@@ -0,0 +1,9 @@
+<nav id="menu">
+	<div class="element_menu">
+		<a class="btn" href="#section">Résultats</a>
+		<a class="btn" href="#section">Statistiques d'équipe</a>
+		<a class="btn" href="#section">Effectif</a>
+		<a class="btn" href="#section">Statistiques individuelles</a>
+		<a class="btn" href="log_admin.php">Administrer</a>
+	</div>
+</nav>
