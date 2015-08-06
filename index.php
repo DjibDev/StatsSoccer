fb9e407ef902f03b6c2d0e14e0c955a6a29200f2
Jerome BLOT <galaxymuse@gmail.com>
2015-08-06 14:40:29 +0200

    index épuré

--- index.php
+++ index.php
@@ -11,17 +11,11 @@
 	
 	<h1>Statistiques</h1>	
 	
-	  <div class="nav">
-		<a class="btn" href="#section">Résultats</a>
-		<a class="btn" href="#section">Statistiques d'équipe</a>
-		<a class="btn" href="#section">Effectif</a>
-		<a class="btn" href="#section">Statistiques individuelles</a>
-		<a class="btn" href="#section">Administrer</a>
-	 </div>
+	 <?php include "menus.php"; ?>
 	
 	
 	<div class="section">
-		<?php include "affiche_effectif.php"; ?>
+		
 	</div>
 		
 
