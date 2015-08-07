06620e6c93200f725c764f4f7eb93cb782b0acb9
Jerome BLOT <galaxymuse@gmail.com>
2015-08-07 16:18:55 +0200

    intégration des blocks HTML5

--- index.php
+++ index.php
@@ -6,17 +6,34 @@
 	<link rel="stylesheet" href="style_base.css"/>
 </head>
 
-
 <body>
 	
-	<h1>Statistiques</h1>	
-	
-	 <?php include "menus.php"; ?>
-	
-	
-	<div class="section">
-		
+<div id="bloc_page">
+	<div id="banniere_image">
+		<h1>Statistiques</h1>	
+		<div id="banniere_description">	
+				<a class="btn" href="#section">Résultats</a>
+				<a class="btn" href="#section">Statistiques d'équipe</a>
+				<a class="btn" href="affiche_effectif.php">Effectif</a>
+				<a class="btn" href="#section">Statistiques individuelles</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				<a class="btn" href="log_admin.php">Administrer</a>
+		</div>
 	</div>
+
+		
+	<section>	
+		<article>
+			<center>
+			<?php include "affiche_effectif.php"; ?>
+			</center>
+		</article>	
+			
+		<aside>
+			<p>Test</p>
+		</aside>
+	</section>
+	
+</div>		
 		
 
 </body>
