fcbbedf42ffd7ae69ac08096355dd2565d6ec8ba
Jerome BLOT <galaxymuse@gmail.com>
2015-08-12 14:17:30 +0200

    fichier obsolete

--- menus.php
+++
@@ -1,9 +0,0 @@
-<nav id="menu">
-	<div class="element_menu">
-		<a class="btn" href="#section">Résultats</a>
-		<a class="btn" href="#section">Statistiques d'équipe</a>
-		<a class="btn" href="#section">Effectif</a>
-		<a class="btn" href="#section">Statistiques individuelles</a>
-		<a class="btn" href="log_admin.php">Administrer</a>
-	</div>
-</nav>
