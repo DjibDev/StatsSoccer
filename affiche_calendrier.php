5d4e9b7e1c34ba7788e0880a0c25617a281b84d0
DjibDev <galaxymuse@gmail.com>
2015-08-15 14:34:37 +0200

    reprise du calendrier apres bug

---
+++ affiche_calendrier.php
@@ -0,0 +1,32 @@
+<html>
+
+<head>
+	<title>Les stats des loisirs</title>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="style_base.css"/>
+</head>
+
+<body>
+	
+<div id="bloc_page">
+<?php
+	include('banniere_menu.php');
+?>	
+		
+
+<section>	
+		<article>
+	    
+		</article>	
+	
+		<aside>
+			<center>
+				<img src="images/petit_logo.png"/>
+			</center>
+			
+		</aside>
+</section>
+	
+
+</body>
+</html>
