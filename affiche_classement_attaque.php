d6b818d8799595e6516f2c73c002278d5c0e8780
DjibDev <galaxymuse@gmail.com>
2015-11-02 11:09:53 +0100

    creation tableau classement attaque et défense

--- affiche_classement_attaque.php
+++ affiche_classement_attaque.php
@@ -27,6 +27,61 @@
 		echo '</table><br>';
 
 		echo '<caption>Classement - Attaque</caption>';	
+		
+		$x=0;
+		echo '<table border=2 cellspacing=2 cellspadding=2 >';
+		echo '<tr class=trheadcolor><th></th><th></th><th width="30">Pts</th><th width="30">J</th><th width="30">V</th><th width="30">N</th><th width="30">D</th><th width="30">F</th><th width="30">Bp</th><th width="30">Bc</th><th width="30">Diff</th></tr>';
+	
+		$req1=$bdd->query('SELECT ID_equipe, nom, favorite, nb_journees, nb_forfaits, nb_victoires, nb_nuls, nb_defaites, nb_buts_pour, nb_buts_contre, diff, points
+		FROM equipes, classement
+		WHERE  equipes.ID_equipe = classement.equipe_id
+		AND nb_journees = (SELECT MAX(nb_journees) FROM classement) 
+		ORDER BY nb_buts_pour DESC, points DESC, diff DESC, nom ASC ');
+		
+		while ($resultats=$req1->fetch())
+		{		
+			// le calcul du modulo de "$x" permet d'alterner le resultat : soit "0" soit "1" 
+			$x++;
+			$altern=$x % 2;
+			$id_equipe=$resultats['ID_equipe'];
+			
+			// affichage différent si s'agit de l'équipe favorite 
+			
+			if ($resultats['favorite'] == true)
+			{
+				$altern='special';
+			}
+			else
+			{	
+				$altern=$x % 2;
+			}
+					
+			echo '<tr class=trcolor'.$altern.'>';
+			echo '<td>'.$x.'</td>';
+			echo '<td><b>'.$resultats['nom'].'</b></td>';
+			echo '<td align="center"><b>'.$resultats['points'].'</b></td>';
+			
+					// requete qui affiche le nombre de match joué par l'équipe
+					$reqjournee=$bdd->query('SELECT COUNT(*) AS NB_J
+					FROM stats_collectives 
+					WHERE equipe_id='.$id_equipe.' ');
+					while ($resultatsnbj=$reqjournee->fetch())
+					{
+						echo '<td align="center">'.$resultatsnbj['NB_J'].'</td>';
+					}
+					$reqjournee->closeCursor();
+					
+			echo '<td align="center">'.$resultats['nb_victoires'].'</td>';
+			echo '<td align="center">'.$resultats['nb_nuls'].'</td>';
+			echo '<td align="center">'.$resultats['nb_defaites'].'</td>';
+			echo '<td align="center">'.$resultats['nb_forfaits'].'</td>';
+			echo '<td align="center">'.$resultats['nb_buts_pour'].'</td>';
+			echo '<td align="center">'.$resultats['nb_buts_contre'].'</td>';
+			echo '<td align="center">'.$resultats['diff'].'</td></tr>';		
+			
+			}
+		$req1->closeCursor();
+		echo '</table><br>';		
                 
     ?>    
 	</center>
