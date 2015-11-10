20b4b979281231469aee8d3e395ac72ed39e0dd2
DjibDev <galaxymuse@gmail.com>
2015-11-10 09:43:19 +0100

    changement du nom menu Accueil -> Home

--- stats_files/banniere_menu_fiche.php
+++ stats_files/banniere_menu_fiche.php
@@ -1,7 +1,7 @@
 <div id="banniere_image">
 		<h1>Saison 2015-2016</h1>	
 		<div id="banniere_description">	
-				<a class="btn" href="../../index.php">Accueil</a>
+				<a class="btn" href="../../index.php">Home</a>
 				<a class="btn" href="../../affiche_calendrier.php">Calendrier/Résultats</a>
 				<a class="btn" href="../../affiche_coupe.php">Coupe</a>
 				<a class="btn" href="../../affiche_classement.php">Classements</a>
