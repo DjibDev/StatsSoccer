ddb9320d6c9c74ebd6650300b82a62ec34a0dfa1
DjibDev <galaxymuse@gmail.com>
2015-12-01 16:45:07 +0100

    modification de l'index

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 ?>	
 	<section>	
 		<article>
-			<p style="background-color:#A7EA95">Le site a vocation de présenter des statistiques simples de la saison (et faire réviser son concepteur !).
-			Il sera complété et agrémenté au fil du temps. 
+			<p style="background-color:#A7EA95">
+			<br>Le site a vocation de présenter des statistiques de la saison de football.
 			<br>
 			<br>
 			Remarque importante: aucune donnée personnelle concernant l'effectif, telle que le nom, le prénom, l'email ou la date de naissance, n'est divulguée. Seuls les pseudos et âges apparaissent.
@@ -26,17 +26,17 @@
 			Pour ceux que ça intéresse, les sources du site sont disponibles <a href=https://github.com/DjibDev/StatsSoccer>ici</a>.
 			<br>
 			<br>
-			
+			</p>
 		</article>	
 		<aside>
 			<center>
 				<br><br>
-				<a href="http://jgefoot.com"><img src="images/petit_logo.png"/></a>
-				<br><br>
+				<h1><i>Logo !</i></h1>
+				<br><br><br>
 			</center>
 		</aside>
 	</section>
-	<?php include ('../footer.php'); ?>
+	<?php include ('footer.php'); ?>
 </div>		
 		
 
