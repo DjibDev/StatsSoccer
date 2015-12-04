355adb0cd9756eb972b1abbe9caba218e3e2de86
DjibDev <galaxymuse@gmail.com>
2015-12-04 16:16:07 +0100

    mise a jour du footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <br>
 	 <footer>
 				<p align="right">Développé par J.BLOT</p>
-				<p align="right">Dernière mise à jour le 03/12/2015</p>
+				<p align="right">Dernière mise à jour le 04/12/2015</p>
       </footer>
