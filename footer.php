fa5e40e6e21fabb40cac04891c72be01f0b3b8fd
DjibDev <galaxymuse@gmail.com>
2015-12-16 12:13:30 +0100

    changement de la fonction qui affiche la derniere Maj du dossier parent

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <br>
 	 <footer>
 				<p align="right">Développé par J.BLOT</p>
-				<p align="right">Dernière mise à jour le 07/12/2015</p>
+				<p align="right">Dernière mise à jour le <?php $getLastModDir = filemtime("../."); echo date("d/m/Y.", $getLastModDir);?></p>
       </footer>
