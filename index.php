1dd88bd5c41c9c42d9a2954780a262fbe9326638
DjibDev <galaxymuse@gmail.com>
2016-01-08 16:24:36 +0100

    matchs a venir ajoutés dans index

--- index.php
+++ index.php
@@ -14,21 +14,23 @@
 ?>	
 	<section>	
 		<article>
+	<?php
+	require ('connexion.php');
+	require ('fonctions_utiles_users.php');
+	?>
 			
-			<h2 align="center">Derniers résultats</h2>
+		<?php 
 
-			<?php 
-			require ('connexion.php');
-			require ('fonctions_utiles_users.php');
+		echo '<h2 align="center">Derniers résultats</h2>';
 			
-		$reponse=$bdd->query('SELECT max(date) as DerJ FROM journees WHERE finished=1');
+		$reponse=$bdd->query('SELECT MAX(date) as DerJ FROM journees WHERE finished=1');
 		$result=$reponse->fetch();
-		echo '<p align="center"><u>Le '.FormatDateFR($result['DerJ']).'</u></p>';
+		echo '<p align="center"><u><b>Le '.FormatDateFR($result['DerJ']).'</b></u></p>';
 		echo '<table align="center">';
 						
 		$reponse2=$bdd->query('SELECT e1.nom equi1, e2.nom equi2, e1.favorite fav1, e2.favorite fav2, equipe_dom_forfait, equipe_dom_penalite, equipe_vis_forfait, equipe_vis_penalite, but_equipe_dom, but_equipe_vis, finished
 		FROM matchs, journees, equipes e1, equipes e2
-		WHERE date=(SELECT max(date) FROM journees WHERE finished=1)
+		WHERE date=(SELECT MAX(date) FROM journees WHERE finished=1)
 		AND journees.ID_journee=matchs.journee_id
 		AND matchs.equipe_dom_id = e1.ID_equipe
 		AND matchs.equipe_vis_id = e2.ID_equipe');
@@ -94,18 +96,61 @@
 
 		
 		echo '</table>';
+		echo '<br>';
+		echo '<br>';
+		echo '<h2 align="center">Matchs à venir</h2>';
+
+		$req=$bdd->query('SELECT MIN(date) as DerJ FROM journees WHERE finished=0');
+		$result2=$req->fetch();
+		echo '<p align="center"><b><u>Le '.FormatDateFR($result2['DerJ']).'</b></u></p>';
+		echo '<table align="center">';
+						
+		$req2=$bdd->query('SELECT e1.nom equi1, e2.nom equi2, e1.favorite fav1, e2.favorite fav2, finished
+		FROM matchs, journees, equipes e1, equipes e2
+		WHERE date=(SELECT MIN(date) FROM journees WHERE finished=0)
+		AND journees.ID_journee=matchs.journee_id
+		AND matchs.equipe_dom_id = e1.ID_equipe
+		AND matchs.equipe_vis_id = e2.ID_equipe');
+	
+		while ($resultats=$req2->fetch())
+		{
+			
+			
+			if ($resultats['fav1'] == true)
+			{
+				echo '<tr class="trcolorspecial"><td width="400" align="center"><b>'.$resultats['equi1'].'</b> - '.$resultats['equi2'].'</td></tr>';
+			}
+			else	
+			{
+				if  ($resultats['fav2'] == true)
+				{
+					echo '<tr class="trcolorspecial"><td width="400" align="center">'.$resultats['equi1'].'  - <b>'.$resultats['equi2'].'</b></td></tr>';
+				}	
+				else
+				{
+					echo '<tr><td width="400" align="center">'.$resultats['equi1'].' - '.$resultats['equi2'].'</td></tr>';
+				}
+			}		
+			
+		}
+		$req2->closeCursor(); 
+
+		
+		echo '</table>';
 				
 			?>
 			<br>
-			<br>
-			<br>
-			<br>
-			
+	
 		</article>	
 		<aside>
 			<center>
 				<br><br>
 				<a href="http://jgefoot.com"><img src="images/petit_logo.png"/></a>
+				<br><br>
+				<br>
+				<br><br>
+				<a href="http://jgefoot.com"><img src="images/ballon.png"/></a>
+				<br><br>
 				<br><br>
 			</center>
 		</aside>
